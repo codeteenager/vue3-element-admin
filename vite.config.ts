d3420d3bff7dee832b73ae4edd5523ab44c47f5a
jiangshuaijie <1830702951@qq.com>
2023-02-13 11:15:34 +0800

    fix: 修改资源相对路径

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   resolve:{
     alias:{
       '@':path.join(__dirname,'src')
